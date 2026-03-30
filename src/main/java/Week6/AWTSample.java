df823cdf3780f42f675992f955ecbdac82ceb0f0
Ed Dela Cruz <edmundo.delacruz@cartrack.com>
2026-03-30 09:37:01 +0800

    Week 6 to 8 Code

---
+++ src/main/java/Week6/AWTSample.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Week6;
+
+import java.awt.*;
+
+/**
+ *
+ * @author Edmundo Dela Cruz
+ */
+public class AWTSample extends Frame{
+    
+    private TextField txtFirstName;
+    private Label lblHeader;
+    private TextArea txaTest;
+    
+    AWTSample(){
+        setSize(400, 400);
+        setLayout(null);
+        
+
+        setVisible(true);
+    }
+    
+}
---
+++ src/main/java/Week6/PolyMorph.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Week6;
+
+/**
+ *
+ * @author Edmundo Dela Cruz
+ */
+public class PolyMorph {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+    
+    public void getName(){
+        System.out.println("Name");
+    }
+    
+    public void getName(String name){
+        System.out.println("Name");
+    }
+    
+    public void getName(int name){
+        System.out.println("Name");
+    }
+}
